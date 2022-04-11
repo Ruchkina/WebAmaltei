26480143711d4812c0a2bf3f76080191704950b3
rgarik <yagarik@yandex.ru>
2022-04-11 08:51:06 +0300

    Add dataset

--- Dto/Response.cs
+++ Dto/Response.cs
@@ -8,12 +8,13 @@ namespace AppAmalt.Dto
     public class Response
     {
         public PortraitDto Portrait { get; set; }
-        public GraphsAgeDto Graphs { get; set; }
+        public GraphsAgeDto GraphsAge { get; set; }
+        public GraphLifeMainDto GraphsLifeMain { get; set; }
 
         public Response(PortraitDto portrait, GraphsAgeDto graphs)
         {
             Portrait = portrait;
-            Graphs = graphs;
+            GraphsAge = graphs;
         }
     }
 }
